8b1cd21141b4a2022dd6d32812a87d57fd37bcaa
prashant <plalwani@purdue.edu>
2013-03-31 16:47:28 -0400

    Adding the cpp file for sat solver

---
+++ sat_solver.cpp
@@ -0,0 +1,24 @@
+/*******************
+*****SAT solver*****
+********************/
+
+// Created By: Prashant Lalwani and Rickard Ewetz
+// Spring 2013
+// Description: Basic SAT solver using DPLL algorithm.
+
+#include <cstdlib>
+#include <fstream>
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+	// If no filename provided, print usage and exit
+	if(argc <= 1)
+	{
+		cout << "Usage:\n" << argv[0] << " <filename>\n";
+		exit(1);
+	}
+	
+return 0;
+}
