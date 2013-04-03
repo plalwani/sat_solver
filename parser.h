e01a92a0165f73cf5318e6aad2ce842673f5a2fc
Rickard F Ewetz <rewetz@ee306lnx3.ecn.purdue.edu>
2013-04-02 20:17:13 -0400

    comments in parser.h

--- parser.h
+++ parser.h
@@ -12,9 +12,11 @@
 //This class in intended to read the inputfile and store it in an array.
 //acces functions can then be used to obtain the input data
 
-//N is the number of brackets (..)(..)  M_i is the number of terms in bracket i.   
-//All indexes start on 0.
-// 0 <= m_i_idx < M_i
+// CLAUSES: N is the number of clauses (..)(..)  
+// TERMS: M_i is the number of terms in clause i.   
+// 
+// Notation: All indexes start on 0.
+
 
 class parser{
   bool readline(std::ifstream &input, std::string &read_to);
@@ -27,7 +29,7 @@ class parser{
 public:
 	parser(std::string inputfile);
 	int numClauses();
-	int numTerms(int bracket_idx); // if there are n brackets bracket idx is valid 0 .. n-1
+	int numTerms(int bracket_idx); //get terms in clasue_idx
 	int getTerm(int bracket_idx,int term_idx);
 	int getVar(); 
 	void print();
