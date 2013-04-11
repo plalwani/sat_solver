ea66710298519045d11a61839b5d8b6a70c37887
Rickard F Ewetz <rewetz@ecelinux34.ecn.purdue.edu>
2013-04-11 17:45:25 -0400

    sat file

--- sat_solver.cpp
+++ sat_solver.cpp
@@ -9,16 +9,31 @@
 #include <cstdlib>
 #include <fstream>
 #include <iostream>
+#include "parser.h"
+#include "problem.h"
+#include "node.h"
+
+parser * P;
+
+
 using namespace std;
 
 int main(int argc, char *argv[])
 {
-	// If no filename provided, print usage and exit
-	if(argc <= 1)
-	{
-		cout << "Usage:\n" << argv[0] << " <filename>\n";
-		exit(1);
-	}
-	
-return 0;
+  // If no filename provided, print usage and exit
+  if(argc <= 1)
+    {
+      cout << "Usage:\n" << argv[0] << " <filename>\n";
+      exit(1);
+    }
+  
+  std::string path = argv[1];
+  std::cout << "PATH: " << path  << "\n";
+  P = new parser(path);
+  P->printData();
+  P->printValue();
+  problem * prob = new problem();
+  prob->solveProblem();  
+
+  return 0;
 }
