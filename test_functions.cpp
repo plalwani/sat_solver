a563d4ed8f741932829cca34694fdc6d01283915
prashant <plalwani@purdue.edu>
2013-04-22 23:07:35 -0400

    Adding the function for watched variables

--- test_functions.cpp
+++ test_functions.cpp
@@ -1,4 +1,4 @@
-// test functions as well as ideas for DPLL, will be added to sat solver after testing
+// Test function to update watch variables and create a sorted list of literals using merge sort
 
 #include <iostream>
 #include <cstdlib>
@@ -16,76 +16,52 @@ F=
 (a'+b'+c)
 */
 
-// this is what the input parser is saving right now
-int dimacs__arr[8][3] = 
-{
-	{-1,2,3},
-	{1,3,4},
-	{1,3,-4},
-	{1,-3,4},
-	{1,-3,-4},
-	{-2,-3,4},
-	{-1,2,-3},
-	{-1,-2,3}
-};
+int dimacs_arr[8][3] = {{-1,2,3},
+{1,3,4},
+{1,3,-4},
+{1,-3,4},
+{1,-3,-4},
+{-2,-3,4},
+{-1,2,-3},
+{-1,-2,3}};
 
-// need to convert it to this form
 // -1 represents complement, 0 represents doesn't exist
-int in_arr[8][4] = 
+int in_arr[8][4] = {{-1,1,1,0},
+{1,0,1,1},
+{1,0,1,-1},
+{1,0,-1,1},
+{1,0,-1,-1},
+{0,-1,-1,1},
+{-1,1,-1,0},
+{-1,-1,1,0}};
+
+int m = 8; // num. of rows
+int n = 4; // num. of cols
+
+int clause_stat[8] = {0,0,0,0,0,0,0,0}; // unsatisfied initially
+int watch_var1[8];	// watched variable 1
+int watch_var2[8];	// watched variable 2
+
+void update_watch_var(int row, int col)
 {
-	{-1,1,1,0},
-	{1,0,1,1},
-	{1,0,1,-1},
-	{1,0,-1,1},
-	{1,0,-1,-1},
-	{0,-1,-1,1},
-	{-1,1,-1,0},
-	{-1,-1,1,0}
-};
-
-/* Do a merge sort of all literals to make variable decision
-Worst case : nlog(n)
-worst case memory: n
-n is the number of variables so it won't be very high anyways
-*/
-
-/*
-end result should look like this :
-{1,3,-3,-1,4,2,-2,-4}
-*/
-
-// count occurence of each literal and save it, sort the literals based on their occurence
+for(int i = 0; i < m; i++)
+{
+	int cnt = 0;
+	for(int j = 0; j < n; j++)
+	{
+		if(clause_stat[i] == 0)
+		{
+			if((dimacs_arr[i][j] != 0)&&(cnt == 0)){watch_var1[i] = dimacs_arr[i][j]; cnt+=1; clause_stat[i] = 2; j+=1;}
+			if((dimacs_arr[i][j] != 0)&&(cnt == 1)){watch_var2[i] = dimacs_arr[i][j]; cnt+=1; clause_stat[i] = 0;}
+			if(cnt == 2){break;}
+		}	
+	}
+}}
 
 int main()
 {
-
-// ASSIGNING VALUES TO LITERALS:
-/*
-start from the literal first in the list
-assign literal=1(use in_arr to decrease computations)
-e.g. 
-for a = 0: 
-	set all -1 entries in first column to a 0
-	set all such rows as GREEN
-	leave all other rows to default YELLOW
-
-for c = 1:
-	set all 1 entries to 0 in column 3 
-	set all such rows as GREEN
-	leave all other rows to defualt YELLOW
-
-Note: just need to read one column depending upon the literal	
-*/
-
-// WATCH VARIABLES:
-/*
-read each row and save the first two non zero elements in memory
-if two variables not available then that row is a unit clause and need to be tagged somehow
-all unit clause rows need to be compared with each other, if any pairs exist then it is a conflictA
-
-OR make next decision based on the unit clause rather than from the sorted list, if any row ends up being 0 then it is a conflict
-*/
-
+update_watch_var(m,n);
+for(int i = 0; i < 8; i++){cout << "\t" << watch_var1[i] << "\t" << watch_var2[i] << "\n";}
 return 0;
 }
 
